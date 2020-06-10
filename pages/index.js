934421440c8fd4985a2e343d31083a2ef5ff4709
afelix <austinjfelix@gmail.com>
2020-06-09 23:46:17 -0400

    quick update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default props => {
         <Grid container justify='center'>
             <Grid item xs={11}>
                 <Card>
-                    <Typography variant="h4">Welcome to FS Notes</Typography>
+                    <Typography variant="h4" align='center'>Welcome to FS Notes</Typography>
                     <Typography variant="body1">
                         This app uses Next.js, powered by Express.js, connected using Apollo and styled with Material-UI
                     </Typography>
